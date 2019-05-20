22b3c487b37bfbe65517c11e4a03d5ac38bd1854
Xiaofei <13263397018@163.com>
2019-05-20 13:13:14 +0800

    add .gitignore

--- src/main/java/com/kl/SocketWindowWordCount.java
+++ src/main/java/com/kl/SocketWindowWordCount.java
@@ -2,7 +2,6 @@ package com.kl;
 
 import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.api.common.functions.ReduceFunction;
-import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
